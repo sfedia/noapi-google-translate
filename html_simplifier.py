f8c47fd73260202ac1b1c9061916a283a12e4337
Fyodor Sizov <fedia.sizov@gmail.com>
2023-03-19 19:38:00 +0300

    added support of regex patches

--- html_simplifier.py
+++ html_simplifier.py
@@ -10,6 +10,30 @@ def copy_index(source_path, dest_path):
     shutil.copy(source_path, dest_path)
 
 
+def regex_modify(dest_path):
+    fl = open(dest_path, encoding="utf-8").read()
+
+    # Merge tables
+    fl = re.sub(
+        (
+            r'<\/table>\s*(?:(?:<p class="[^"]+"><span class="target-text">\d*<\/span><\/p>|'
+            r'<a id="[^"]+"><\/a>)\n?)+\n*<table class="[^"]+">\n*'
+        ),
+    "", fl)
+    print("Applied 'merge tables' patch")
+
+    # Remove page numbers
+    fl = re.sub(r'\n<p class="[^"]+"><span class="target-text">\d+\s*<\/span><\/p>', "", fl)
+    print("Applied 'remove page numbers' patch")
+
+    # з.е. -> credits
+    fl = re.sub(r'з[.][ ]*е[.]', 'зачётные единицы', fl)
+    print("Applied 'з.е. -> credits' patch")
+    with open(dest_path, "w", encoding="utf-8") as fl_to_write:
+        fl_to_write.write(fl)
+        fl_to_write.close()
+
+
 def simplify_html(dest_path):
     tree = lxml.html.parse(dest_path)
     for p_tag in tree.xpath('body/p'):
@@ -41,6 +65,7 @@ if __name__ == "__main__":
     parser.add_argument("--source-name")
     parser.add_argument("--dest-folder")
     parser.add_argument("--dest-name")
+    parser.add_argument("--regex-patches")
 
     args = parser.parse_args()
 
@@ -49,14 +74,18 @@ if __name__ == "__main__":
         source_name = input("Source name: ")
         dest_folder = input("Destination folder: ")
         dest_name = input("Destination name: ")
+        regex_mod = input("Apply regex patches [n]? ") == "y"
     else:
         source_folder = args.source_folder
         source_name = args.source_name
         dest_folder = args.dest_folder
         dest_name = args.dest_name
+        regex_mod = args.regex_patches
 
     source_path = os.path.join(source_folder, source_name)
     dest_path = os.path.join(dest_folder, dest_name)
     copy_index(source_path, dest_path)
     simplify_html(dest_path)
     print(f"Simplified '{source_path}' -> '{dest_path}'")
+    if regex_mod:
+        regex_modify(dest_path)
